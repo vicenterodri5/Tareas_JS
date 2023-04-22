59a587e26ff9f8a5c6fe143979273ac99224c64d
vicenterodri5 <131320910+vicenterodri5@users.noreply.github.com>
2023-04-22 17:56:38 -0600

    Add files via upload

---
+++ Tarea_1/Tarea1.js
@@ -0,0 +1,7 @@
+function difference(a, b) {
+  return a - b;
+}
+
+// Ejemplo de uso:
+const result = difference(10, 3);
+console.log("La diferencia es: " + result); // Resultado: La diferencia es: 7
